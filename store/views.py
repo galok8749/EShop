4a74f14a6158e2f5b4201b7db82b7c56fb9c00ca
alok gupta <galok8749@gmail.com>
2021-06-22 11:26:32 +0530

    categories added

--- store/views.py
+++ store/views.py
@@ -3,10 +3,14 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models.product import Product
+from .models.category import Category
 # Create your views here.
 
 
 def index(request):
     products = Product.get_all_products()
-    # print(products)
-    return render(request, 'index.html', {'products' : products})
+    categories = Category.get_all_categories()
+    data = {}
+    data['products'] = products
+    data['categories'] = categories
+    return render(request, 'index.html', data)
