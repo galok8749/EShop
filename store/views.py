9a90735de768738c49adef095ab240864a622d4e
alok gupta <galok8749@gmail.com>
2021-06-29 16:11:25 +0530

    login

--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from django import http
 from store.models.product import Product
 from django.http.response import HttpResponse
 from django.shortcuts import render, redirect
@@ -85,3 +86,23 @@ def signup(request):
     else:
         return registerUser(request)
     
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    else:
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        customer = Customer.get_customer_by_email(email)
+        
+        error_message = None
+        if customer:
+            flag =  check_password(password, customer.password)
+            if flag:
+                return redirect('homepage')
+            else:
+                error_message = 'Email or Password invalid !!!'
+        else:
+            error_message = 'Email or Password invalid !!!'
+
+        return render(request, 'login.html', {'error' : error_message})
