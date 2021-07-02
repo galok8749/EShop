72c9ab0611c001d3efc970ae145f8fe3e2cc9c97
alok gupta <galok8749@gmail.com>
2021-07-02 10:01:05 +0530

    cart added

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views.home import index
+from .views.home import Index
 from .views.login import Login
 from .views.signup import Signup
 
 urlpatterns = [
-    path('', index, name='homepage'),
+    path('', Index.as_view(), name='homepage'),
     path('signup/',  Signup.as_view(), name='signup'),
     path('login/',  Login.as_view(), name='login')
 ]
--- store/views/home.py
+++ store/views/home.py
@@ -1,19 +1,25 @@
-from store.models.product import Product
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from store.models.product import Product
 from store.models.category import Category
+from django.views import View
 
-# Create your views here.
-def index(request):
-    products = None
-    categories = Category.get_all_categories()
-    categoryID = request.GET.get('category')
+class Index(View):
+
+    def post(self, request):
+        product = request.POST.get('product')
+        return redirect('homepage')
+
+    def get(self, request):
+        products = None
+        categories = Category.get_all_categories()
+        categoryID = request.GET.get('category')
+        
+        if categoryID:
+            products = Product.get_all_products_by_categoryid(categoryID)
+        else:
+            products = Product.get_all_products()
+        data = {}
+        data['products'] = products
+        data['categories'] = categories
+        return render(request, 'index.html', data)
     
-    if categoryID:
-        products = Product.get_all_products_by_categoryid(categoryID)
-    else:
-        products = Product.get_all_products()
-    data = {}
-    data['products'] = products
-    data['categories'] = categories
-    return render(request, 'index.html', data)
