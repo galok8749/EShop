50a9c2c6421057cb8758dc31765b1235c49fbf3a
alok gupta <galok8749@gmail.com>
2021-07-02 12:05:13 +0530

    cart added

--- store/templatetags/cart.py
+++ store/templatetags/cart.py
@@ -10,3 +10,12 @@ def is_in_cart(product, cart):
         if int(id) == product.id:
             return True
     return False
+
+@register.filter(name='cart_quantity')
+def cart_quantity(product, cart):
+    keys = cart.keys()
+    # product is in cart
+    for id in keys:
+        if int(id) == product.id:
+            return cart.get(id)
+    return False
