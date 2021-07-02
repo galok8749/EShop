ab144c7ef4c692a555bb1a661037914e1b4122d0
alok gupta <galok8749@gmail.com>
2021-07-02 10:23:04 +0530

    cart added

--- store/views/home.py
+++ store/views/home.py
@@ -7,6 +7,19 @@ class Index(View):
 
     def post(self, request):
         product = request.POST.get('product')
+        cart = request.session.get('cart')
+        if cart:
+            quantity = cart.get(product)
+            if quantity:
+                cart[product] = quantity+1
+            else:
+                cart[product] = 1
+        else:
+            cart = {}
+            cart[product] = 1
+        
+        request.session['cart'] = cart
+        print(request.session['cart'])
         return redirect('homepage')
 
     def get(self, request):
