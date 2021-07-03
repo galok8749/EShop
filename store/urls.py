a5e99528c6b13297986626bbe22480ebba93211b
alok gupta <galok8749@gmail.com>
2021-07-03 16:52:47 +0530

    card added

--- store/urls.py
+++ store/urls.py
@@ -3,10 +3,12 @@ from django.views.generic.base import View
 from .views.home import Index
 from .views.login import Login, logout
 from .views.signup import Signup
+from .views.cart import Cart
 
 urlpatterns = [
     path('', Index.as_view(), name='homepage'),
     path('signup/',  Signup.as_view(), name='signup'),
     path('login/',  Login.as_view(), name='login'),
     path('logout/',  logout, name='logout'),
+    path('cart/',  Cart.as_view(), name='cart'),
 ]
