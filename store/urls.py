e4f05228a378a3808d698c562a56a2363b06f478
alok gupta <galok8749@gmail.com>
2021-07-03 16:30:22 +0530

    logout added

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.views.generic.base import View
 from .views.home import Index
-from .views.login import Login
+from .views.login import Login, logout
 from .views.signup import Signup
 
 urlpatterns = [
     path('', Index.as_view(), name='homepage'),
     path('signup/',  Signup.as_view(), name='signup'),
-    path('login/',  Login.as_view(), name='login')
+    path('login/',  Login.as_view(), name='login'),
+    path('logout/',  logout, name='logout'),
 ]
--- store/views/login.py
+++ store/views/login.py
@@ -17,8 +17,7 @@ class Login(View):
         if customer:
             flag =  check_password(password, customer.password)
             if flag:
-                request.session['customer_id'] = customer.id
-                request.session['email'] = customer.email
+                request.session['customer'] = customer.id
                 return redirect('homepage')
             else:
                 error_message = 'Email or Password invalid !!!'
@@ -26,3 +25,8 @@ class Login(View):
             error_message = 'Email or Password invalid !!!'
 
         return render(request, 'login.html', {'error' : error_message})
+
+
+def logout(request):
+    request.session.clear()
+    return redirect('login')
